d8693c63cd6da7cdd114c0ac7fe3f7203aac978b
amith.nair <amith.nair@talabat.com>
2023-02-05 12:00:41 +0400

    pass test

--- expensereport-csharp/expensereport-ut/ExpenseReportTest.cs
+++ expensereport-csharp/expensereport-ut/ExpenseReportTest.cs
@@ -21,6 +21,23 @@ namespace Tests
             Assert.AreEqual("Meal expenses: 0",report._messages[1]);
             Assert.AreEqual("Total expenses: 0",report._messages[2]);
         }
+        
+        [Test]
+        public void ExpenseReportForDinerExpense()
+        {
+            var now = DateTime.Now;
+            var report = new DummyExpenseReport
+            {
+                _dateTime = now
+            };
+
+            var dinnerExpense = new Expense(){amount = 10,type = ExpenseType.DINNER};
+            report.PrintReport(new List<Expense>() { dinnerExpense });
+            Assert.AreEqual($"Expenses {now}",report._messages[0]);
+            Assert.AreEqual("Dinner\t10\t ",report._messages[1]);
+            Assert.AreEqual("Meal expenses: 10",report._messages[2]);
+            Assert.AreEqual("Total expenses: 10",report._messages[3]);
+        }
 
         private class DummyExpenseReport: ExpenseReport
         {
