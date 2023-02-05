80494e0df9f034d582e88c3d687a1c3b272536eb
Pankaj Sharma <pankaj.sharma@talabat.com>
2023-02-05 15:48:13 +0400

    remove enum

--- expensereport-csharp/expensereport-csharp/ExpenseType.cs
+++ expensereport-csharp/expensereport-csharp/ExpenseType.cs
@@ -2,27 +2,18 @@ namespace expensereport_csharp
 {
     public class ExpenseType
     {
-        private enum Type
-        {
-            DINNER,
-            BREAKFAST,
-            CAR_RENTAL
-        }
+        public static readonly ExpenseType Dinner = new("Dinner", true, 5000);
 
-        public static readonly ExpenseType Dinner = new(Type.DINNER, "Dinner", true, 5000);
+        public static readonly ExpenseType Breakfast = new("Breakfast", true, 1000);
 
-        public static readonly ExpenseType Breakfast = new(Type.BREAKFAST, "Breakfast", true, 1000);
+        public static readonly ExpenseType CarRental = new("Car Rental", false, int.MaxValue);
 
-        public static readonly ExpenseType CarRental = new(Type.CAR_RENTAL, "Car Rental", false, int.MaxValue);
-
-        private readonly Type _type;
         private readonly string _name;
         private readonly bool _isMeal;
         private readonly int _amountLimit;
 
-        private ExpenseType(Type type, string name, bool isMeal, int amountLimit)
+        private ExpenseType(string name, bool isMeal, int amountLimit)
         {
-            _type = type;
             _name = name;
             _isMeal = isMeal;
             _amountLimit = amountLimit;
