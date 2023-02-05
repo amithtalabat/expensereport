45633c2cbc532180ac73957f1daf15e652109a70
amith.nair <amith.nair@talabat.com>
2023-02-05 15:55:18 +0400

    xm

--- expensereport-csharp/expensereport-csharp/Class1.cs
+++ expensereport-csharp/expensereport-csharp/Class1.cs
@@ -9,30 +9,57 @@ namespace expensereport_csharp
 
         public void PrintReport(List<Expense> expenses)
         {
-            var total = 0;
-            var mealExpenses = 0;
-
             Print("Expenses " + DateTime);
 
-            foreach (var expense in expenses)
+            var expensesDomain = new Expenses(expenses);
+
+            foreach (var expense in expensesDomain._expenses)
+            {
+                Print(expense.Name() + "\t" + expense.Amount + "\t" + expense.MealLimitMarker());
+            }
+
+            Print("Meal expenses: " + MealExpenses(expensesDomain));
+            Print("Total expenses: " + expensesDomain.Total());
+        }
+
+        private static int MealExpenses(Expenses expensesDomain)
+        {
+            var mealExpenses = 0;
+            foreach (var expense in expensesDomain._expenses)
             {
                 if (expense.IsMeal())
                 {
                     mealExpenses += expense.Amount;
                 }
-
-                Print(expense.Name() + "\t" + expense.Amount + "\t" + expense.MealLimitMarker());
-
-                total += expense.Amount;
             }
 
-            Print("Meal expenses: " + mealExpenses);
-            Print("Total expenses: " + total);
+            return mealExpenses;
         }
 
         protected virtual void Print(string printedMessage)
         {
             Console.WriteLine(printedMessage);
+        }
+    }
+
+    public class Expenses
+    {
+        public List<Expense> _expenses;
+
+        public Expenses(List<Expense> expenses)
+        {
+            _expenses = expenses;
+        }
+
+        public int Total()
+        {
+            var total = 0;
+            foreach (var expense in _expenses)
+            {
+                total += expense.Amount;
+            }
+
+            return total;
         }
     }
 }
