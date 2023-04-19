be59e136cd16847c1e6611082f0bb3b34047fa1a
Pankaj Sharma <pankaj.sharma@talabat.com>
2023-04-19 17:29:45 +0400

    test for no expenses

--- expensereport-csharp/expensereport-ut/ExpenseReportTests.cs
+++ expensereport-csharp/expensereport-ut/ExpenseReportTests.cs
@@ -1,3 +1,7 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using expensereport_csharp;
 using NUnit.Framework;
 
 namespace Tests
@@ -5,9 +9,18 @@ namespace Tests
     public class ExpenseReportTests
     {
         [Test]
-        public void Test1()
+        public void NoExpenses()
         {
-            Assert.Pass();
+            var output = new StringWriter();
+            Console.SetOut(output);
+            
+            var report = new ExpenseReport();
+            var expenses = new List<Expense>();
+            
+            report.PrintReport(expenses);
+            Assert.AreEqual("Expenses " + DateTime.Now + Environment.NewLine +
+                            "Meal expenses: 0" + Environment.NewLine +
+                            "Total expenses: 0" + Environment.NewLine, output.ToString());
         }
     }
 }
