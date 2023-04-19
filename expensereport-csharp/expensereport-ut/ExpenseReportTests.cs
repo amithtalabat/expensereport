7d1ef99ba718d58ca7eacf533ee9747bc7be63d6
amith.nair <amith.nair@talabat.com>
2023-04-19 17:39:39 +0400

    Revert "test for no expenses"
    This reverts commit be59e136cd16847c1e6611082f0bb3b34047fa1a.

--- expensereport-csharp/expensereport-ut/ExpenseReportTests.cs
+++ expensereport-csharp/expensereport-ut/ExpenseReportTests.cs
@@ -1,7 +1,3 @@
-using System;
-using System.Collections.Generic;
-using System.IO;
-using expensereport_csharp;
 using NUnit.Framework;
 
 namespace Tests
@@ -9,18 +5,9 @@ namespace Tests
     public class ExpenseReportTests
     {
         [Test]
-        public void NoExpenses()
+        public void Test1()
         {
-            var output = new StringWriter();
-            Console.SetOut(output);
-            
-            var report = new ExpenseReport();
-            var expenses = new List<Expense>();
-            
-            report.PrintReport(expenses);
-            Assert.AreEqual("Expenses " + DateTime.Now + Environment.NewLine +
-                            "Meal expenses: 0" + Environment.NewLine +
-                            "Total expenses: 0" + Environment.NewLine, output.ToString());
+            Assert.Pass();
         }
     }
 }
