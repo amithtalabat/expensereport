59a3f3d1e4d9d685f5c9ec75f57d44553dc5dba8
amith.nair <amith.nair@talabat.com>
2023-04-19 17:43:08 +0400

    dinner expense

--- expensereport-csharp/expensereport-ut/ExpenseReportTests.cs
+++ expensereport-csharp/expensereport-ut/ExpenseReportTests.cs
@@ -20,5 +20,22 @@ namespace Tests
                             "Meal expenses: 0" + Environment.NewLine +
                             "Total expenses: 0" + Environment.NewLine, output.ToString());
         }
+
+        [Test]
+        public void DinnerExpense()
+        {
+            var report = new ExpenseReport();
+            var expenses = new List<Expense>
+            {
+                new Expense {type = ExpenseType.DINNER, amount = 5000}
+            };
+            var output = new StringWriter();
+            Console.SetOut(output);
+            report.PrintReport(expenses);
+            Assert.AreEqual("Expenses " + DateTime.Now + Environment.NewLine +
+                            "Dinner\t5000\t " + Environment.NewLine +
+                            "Meal expenses: 5000" + Environment.NewLine +
+                            "Total expenses: 5000" + Environment.NewLine, output.ToString());
+        }
     }
 }
