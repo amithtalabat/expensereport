1101343e1a642db59daea6e1768afd302ceea23b
amith.nair <amith.nair@talabat.com>
2023-06-20 13:14:05 +0400

    xm

--- expensereport-csharp/expensereport-csharp/Class1.cs
+++ expensereport-csharp/expensereport-csharp/Class1.cs
@@ -77,18 +77,14 @@ namespace expensereport_csharp
         public static void PrintReport(List<Expense> expenses)
         {
             var total = 0;
-            var mealExpenses = 0;
 
             Console.WriteLine("Expenses " + DateTime.Now);
-            
-            foreach (var expense in expenses)
-            {
-                if (expense.IsMeal())
-                {
-                    mealExpenses += expense.amount;
-                }
 
-                var expenseName = expense.Name();
+            var mealExpenses = MealExpenses(expenses);
+
+            foreach (var expense in expenses){
+
+            var expenseName = expense.Name();
 
                 var mealOverExpensesMarker = expense.IsMealOverExpense()
                         ? MealOverExpensesMarker : NoMarker;
@@ -100,6 +96,20 @@ namespace expensereport_csharp
 
             Console.WriteLine("Meal expenses: " + mealExpenses);
             Console.WriteLine("Total expenses: " + total);
+        }
+
+        private static int MealExpenses(List<Expense> expenses)
+        {
+            var mealExpenses = 0;
+            foreach (var expense in expenses)
+            {
+                if (expense.IsMeal())
+                {
+                    mealExpenses += expense.amount;
+                }
+            }
+
+            return mealExpenses;
         }
 
         private static string ExpenseWithMarker(string expenseName, Expense expense, string mealOverExpensesMarker)
