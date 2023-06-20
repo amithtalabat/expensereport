bf65970eedd105420617a1e7d6fc0ba0c2218b89
eduar.rodriguez <eduar.rodriguez@talabat.com>
2023-06-20 12:48:55 +0400

    Format braces

--- expensereport-csharp/expensereport-csharp/Class1.cs
+++ expensereport-csharp/expensereport-csharp/Class1.cs
@@ -74,5 +74,6 @@ namespace expensereport_csharp
 
             Console.WriteLine("Meal expenses: " + mealExpenses);
             Console.WriteLine("Total expenses: " + total);
-        }    }
+        }    
+    }
 }
